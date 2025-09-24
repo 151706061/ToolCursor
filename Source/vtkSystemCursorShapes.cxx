c1a7118f6b850a40e51f8e84ab3409642db3e0aa
ystarrev911 <231239124+ystarrev911@users.noreply.github.com>
2025-09-23 20:29:01 -0600

    VTK9 changes

--- Source/vtkSystemCursorShapes.cxx
+++ Source/vtkSystemCursorShapes.cxx
@@ -67,8 +67,8 @@ vtkDataSet *vtkSystemCursorShapes::MakePointerShape()
   vtkCellArray *strips = vtkCellArray::New();
   vtkCellArray *lines = vtkCellArray::New();
 
-  static unsigned char black[4] = {  0,   0,   0, 255};
-  static unsigned char white[4] = {255, 255, 255, 255};
+  static float black[3] = {  0.0,   0.0,   0.0};
+  static float white[3] = {255.0, 255.0, 255.0};
 
   static double hotspot[2] = { 0.5, -0.5 };
   static double coords[7][2] = {
@@ -98,21 +98,21 @@ vtkDataSet *vtkSystemCursorShapes::MakePointerShape()
   {
     points->InsertNextPoint(coords[i][0] - hotspot[0],
                             coords[i][1] - hotspot[1], 0.0);
-    scalars->InsertNextTupleValue(white);
+    scalars->InsertNextTuple(white);
   }
 
   for (int j = 0; j < 7; j++)
   {
     points->InsertNextPoint(coords[j][0] - hotspot[0],
                             coords[j][1] - hotspot[1], +0.1);
-    scalars->InsertNextTupleValue(black);
+    scalars->InsertNextTuple(black);
   }
 
   for (int k = 0; k < 7; k++)
   {
     points->InsertNextPoint(coords[k][0] - hotspot[0],
                             coords[k][1] - hotspot[1], -0.1);
-    scalars->InsertNextTupleValue(black);
+    scalars->InsertNextTuple(black);
   }
 
   // Make the strips
@@ -141,109 +141,89 @@ vtkDataSet *vtkSystemCursorShapes::MakePointerShape()
 //----------------------------------------------------------------------------
 vtkDataSet *vtkSystemCursorShapes::MakeCrosshairShape()
 {
-  vtkUnsignedCharArray *scalars = vtkUnsignedCharArray::New();
-  scalars->SetNumberOfComponents(4);
-  vtkPoints *points = vtkPoints::New();
-  vtkCellArray *strips = vtkCellArray::New();
-  vtkCellArray *lines = vtkCellArray::New();
+    vtkNew<vtkUnsignedCharArray> scalars;
+    scalars->SetNumberOfComponents(4);
+    scalars->SetName("Colors");
 
-  static unsigned char black[4] = {  0,   0,   0, 255};
-  static unsigned char white[4] = {255, 255, 255, 255};
+    vtkNew<vtkPoints> points;
+    vtkNew<vtkCellArray> strips;
+    vtkNew<vtkCellArray> lines;
 
-  const double radius = 8;
-  const double inner = 3;
+    static unsigned char black[4] = { 0, 0, 0, 255 };
+    static unsigned char white[4] = { 255, 255, 255, 255 };
 
-  static double coords[8][2] = {
-    { 0, -radius }, { 0, -inner }, { 0, +inner }, { 0, +radius },
-    { -radius, 0 }, { -inner, 0 }, { +inner, 0 }, { +radius, 0 },
-  };
+    const double radius = 8;
+    const double inner = 3;
 
-  static double outCoords[16][2] = {
-    { -1, -radius-1 }, { +1, -radius-1 }, { +1, -inner+1 }, { -1, -inner+1 },
-    { +1, +radius+1 }, { -1, +radius+1 }, { -1, +inner-1 }, { +1, +inner-1 },
-    { -radius-1, +1 }, { -radius-1, -1 }, { -inner+1, -1 }, { -inner+1, +1 },
-    { +radius+1, -1 }, { +radius+1, +1 }, { +inner-1, +1 }, { +inner-1, -1 },
-  };
+    static double coords[8][2] = {
+        {0, -radius}, {0, -inner}, {0, +inner}, {0, +radius},
+        {-radius, 0}, {-inner, 0}, {+inner, 0}, {+radius, 0},
+    };
 
-  static vtkIdType toplineIds[] = {
-    2, 0, 1,
-    2, 2, 3,
-    2, 4, 5,
-    2, 6, 7,
-  };
+    static double outCoords[16][2] = {
+        {-1, -radius-1}, {+1, -radius-1}, {+1, -inner+1}, {-1, -inner+1},
+        {+1, +radius+1}, {-1, +radius+1}, {-1, +inner-1}, {+1, +inner-1},
+        {-radius-1, +1}, {-radius-1, -1}, {-inner+1, -1}, {-inner+1, +1},
+        {+radius+1, -1}, {+radius+1, +1}, {+inner-1, +1}, {+inner-1, -1},
+    };
 
-  static vtkIdType botlineIds[] = {
-    2, 8, 9,
-    2, 10, 11,
-    2, 12, 13,
-    2, 14, 15,
-  };
+    static vtkIdType toplineIds[] = {2, 0, 1, 2, 2, 3, 2, 4, 5, 2, 6, 7};
+    static vtkIdType botlineIds[] = {2, 8, 9, 2, 10, 11, 2, 12, 13, 2, 14, 15};
+    static vtkIdType outlineIds[] = {
+        5, 16, 17, 18, 19, 16, 5, 20, 21, 22, 23, 20,
+        5, 24, 25, 26, 27, 24, 5, 28, 29, 30, 31, 28
+    };
+    static vtkIdType stripIds[] = {
+        4, 16, 17, 19, 18, 4, 20, 21, 23, 22, 4, 24, 25, 27, 26, 4, 28, 29, 31, 30
+    };
 
-  static vtkIdType outlineIds[] = {
-    5, 16, 17, 18, 19, 16,
-    5, 20, 21, 22, 23, 20,
-    5, 24, 25, 26, 27, 24,
-    5, 28, 29, 30, 31, 28,
-  };
+    // Insert crosshair points
+    for (int i = 0; i < 8; i++)
+    {
+        points->InsertNextPoint(coords[i][0] + 0.5, coords[i][1] - 0.5, +0.1);
+        scalars->InsertNextTypedTuple(black);
+    }
 
-  static vtkIdType stripIds[] = {
-    4, 16, 17, 19, 18,
-    4, 20, 21, 23, 22,
-    4, 24, 25, 27, 26,
-    4, 28, 29, 31, 30,
-  };
+    for (int j = 0; j < 8; j++)
+    {
+        points->InsertNextPoint(coords[j][0] + 0.5, coords[j][1] - 0.5, -0.1);
+        scalars->InsertNextTypedTuple(black);
+    }
 
-  for (int i = 0; i < 8; i++)
-  {
-    points->InsertNextPoint(coords[i][0]+0.5, coords[i][1]-0.5, +0.1);
-    scalars->InsertNextTupleValue(black);
-  }
+    for (int k = 0; k < 16; k++)
+    {
+        points->InsertNextPoint(outCoords[k][0] + 0.5, outCoords[k][1] - 0.5, 0);
+        scalars->InsertNextTypedTuple(white);
+    }
 
-  for (int j = 0; j < 8; j++)
-  {
-    points->InsertNextPoint(coords[j][0]+0.5, coords[j][1]-0.5, -0.1);
-    scalars->InsertNextTupleValue(black);
-  }
+    // Make the crosshairs
+    lines->InsertNextCell(2, &toplineIds[1]);
+    lines->InsertNextCell(2, &toplineIds[4]);
+    lines->InsertNextCell(2, &toplineIds[7]);
+    lines->InsertNextCell(2, &toplineIds[10]);
 
-  for (int k = 0; k < 16; k++)
-  {
-    points->InsertNextPoint(outCoords[k][0]+0.5, outCoords[k][1]-0.5, 0);
-    scalars->InsertNextTupleValue(white);
-  }
+    lines->InsertNextCell(2, &botlineIds[1]);
+    lines->InsertNextCell(2, &botlineIds[4]);
+    lines->InsertNextCell(2, &botlineIds[7]);
+    lines->InsertNextCell(2, &botlineIds[10]);
 
-  // Make the crosshairs
-  lines->InsertNextCell(toplineIds[0], &toplineIds[1]);
-  lines->InsertNextCell(toplineIds[3], &toplineIds[4]);
-  lines->InsertNextCell(toplineIds[6], &toplineIds[7]);
-  lines->InsertNextCell(toplineIds[9], &toplineIds[10]);
+    // Make the outline
+    lines->InsertNextCell(5, &outlineIds[1]);
+    lines->InsertNextCell(5, &outlineIds[7]);
+    lines->InsertNextCell(5, &outlineIds[13]);
+    lines->InsertNextCell(5, &outlineIds[19]);
 
-  // Make the crosshairs
-  lines->InsertNextCell(botlineIds[0], &botlineIds[1]);
-  lines->InsertNextCell(botlineIds[3], &botlineIds[4]);
-  lines->InsertNextCell(botlineIds[6], &botlineIds[7]);
-  lines->InsertNextCell(botlineIds[9], &botlineIds[10]);
+    // Fill the outline
+    strips->InsertNextCell(4, &stripIds[1]);
+    strips->InsertNextCell(4, &stripIds[6]);
+    strips->InsertNextCell(4, &stripIds[11]);
+    strips->InsertNextCell(4, &stripIds[16]);
 
-  // Make the outline
-  lines->InsertNextCell(outlineIds[0], &outlineIds[1]);
-  lines->InsertNextCell(outlineIds[6], &outlineIds[7]);
-  lines->InsertNextCell(outlineIds[12], &outlineIds[13]);
-  lines->InsertNextCell(outlineIds[18], &outlineIds[19]);
+    vtkNew<vtkPolyData> data;
+    data->SetPoints(points);
+    data->SetLines(lines);
+    data->SetStrips(strips);
+    data->GetPointData()->SetScalars(scalars);
 
-  // Fill the outline
-  strips->InsertNextCell(stripIds[0], &stripIds[1]);
-  strips->InsertNextCell(stripIds[5], &stripIds[6]);
-  strips->InsertNextCell(stripIds[10], &stripIds[11]);
-  strips->InsertNextCell(stripIds[15], &stripIds[16]);
-
-  vtkPolyData *data = vtkPolyData::New();
-  data->SetPoints(points);
-  points->Delete();
-  data->SetLines(lines);
-  lines->Delete();
-  data->SetStrips(strips);
-  strips->Delete();
-  data->GetPointData()->SetScalars(scalars);
-  scalars->Delete();
-
-  return data;
+    return data;
 }
